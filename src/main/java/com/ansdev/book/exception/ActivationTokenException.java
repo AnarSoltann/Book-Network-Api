2333f59700d80a79bcd3c96320b0760291a87403
AnarSoltann <anar.soltann@gmail.com>
2024-07-30 04:49:53 +0400

    update

--- src/main/java/com/ansdev/book/exception/ActivationTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.ansdev.book.exception;
-
-public class ActivationTokenException extends RuntimeException {
-    public ActivationTokenException(String message) {
-        super(message);
-    }
-}
